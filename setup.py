cbfc7a04529f7f9f97209649f5599281ae8cb59d
arvind-sgvu <n8n.arvind@gmail.com>
2026-06-02 19:13:35 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Arvind Singh",
+    author_email="n8n.arvind@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
